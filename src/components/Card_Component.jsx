3c009c76ae0fe5d4becdd0b25c09ce7b1ed6a9cc
DAngelo Watson <dangelo.watson1212@gmail.com>
2023-10-05 00:28:31 -0400

    first commit

---
+++ src/components/Card_Component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';  // Import React from 'react'
+
+import '../components/card.css';
+
+const CardComponent = () => {
+  return (
+    <div className="card-wrapper">
+        <div>text</div>
+    </div>
+  );
+}
+
+export default CardComponent;  // Export as CardComponent
