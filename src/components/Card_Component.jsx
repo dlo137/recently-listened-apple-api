da8616a947941f2e4060fa8a2a4a55a9a3f2947c
DAngelo Watson <dangelo.watson1212@gmail.com>
2023-10-05 14:39:52 -0400

    header & body styling, content added

--- src/components/Card_Component.jsx
+++ src/components/Card_Component.jsx
@@ -1,14 +1,65 @@
-import React from 'react';  // Import React from 'react'
+import React from 'react';  
 
 import '../components/card.css';
 import appleIcon from '../images/apple.png';
+import rewindIcon from '../images/rewind.png'
+import playIcon from '../images/play.png'
+import fowardIcon from '../images/foward.png'
+import nextIcon from '../images/next.png'
+
 
 const CardComponent = () => {
   return (
     <div className="card-wrapper">
-      <div className='logo-container'>
+      {/* ======================= APPLE LOGO ======================= */}
+      <header className='logo-container'>
         <img src={appleIcon} alt='apple icon'/>
-      </div>
+      </header>
+
+      <body>
+        {/* ======================= LEFT SIDE OF CARD COMPENENT ======================= */}
+        <div className='left-container'>
+          <div className='album-cover'>
+          </div>
+        </div>
+
+        {/* ======================= RIGHT SIDE OF CARD COMPENENT ======================= */}
+        <div className='right-container'>
+          {/* ======================= SONG INFORMATION ======================= */}
+          <div className='song-info'>
+            <div>
+              <h1>Recently Played</h1>
+            </div>
+
+            <div className='song-artist'>
+              <p>Song</p>
+              <p>Artist</p>
+            </div>
+          </div>
+
+          {/* ======================= SOUND BAR ======================= */}
+          <div className='soundbar-container'>
+            <div className='soundbar-icons'>
+              soundbar icons
+            </div>
+
+            <div className='duration-line'>
+              line
+            </div>
+
+            <div className='song-time'>
+              <p>0:00</p>
+              <p>3:33</p>
+            </div>
+          </div>
+        </div>
+
+        
+
+
+      </body>
+
+      
     </div>
   );
 }
