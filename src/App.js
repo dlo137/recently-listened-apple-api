6d81f0804abf1a89e3a18bff3ed2690bc085e4df
DAngelo Watson <dangelo.watson1212@gmail.com>
2023-10-08 13:47:41 -0400

    UI finished

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <div className='Wrapper'>
-        <CardComponent />  {/* Use PascalCase for the component */}
+        <CardComponent />
       </div>
     </div>
   );
